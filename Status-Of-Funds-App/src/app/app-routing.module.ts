52bbe3c0f5d23b5d9ec3f1045068932b28f32134
MathiewTackitt <mathiewtackitt@gmail.com>
2022-08-31 11:59:14 -0400

    Adds shield logo to login screen and a title for the page. Adds login button. No functionality yet.

--- Status-Of-Funds-App/src/app/app-routing.module.ts
+++ Status-Of-Funds-App/src/app/app-routing.module.ts
@@ -3,14 +3,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   },
+
 ];
 
 @NgModule({
