0eb2da43838ab0edc6d8ffe45a6d918411f59b3b
MathiewTackitt <mathiewtackitt@gmail.com>
2022-10-13 12:50:13 -0400

    Turns settings page into modal

--- Status-Of-Funds-App/src/app/components/header/header.component.ts
+++ Status-Of-Funds-App/src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular'
 
 @Component({
   selector: 'app-header',
@@ -10,8 +11,14 @@ export class HeaderComponent implements OnInit {
   @Input() title: string;
   @Input() goBack: boolean;
 
+  @ViewChild(IonModal) modal: IonModal;
+
   constructor() { }
 
   ngOnInit() {}
+
+  close() {
+    this.modal.dismiss(null, 'close');
+  }
 
 }
