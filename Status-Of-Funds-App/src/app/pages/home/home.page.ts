7cbdaf9c2d2f5020b61a285b479d229f7dd2c3b6
Vinhky Nguyen~er-sama <vinhkynguyen235@gmail.com>
2022-10-31 12:33:29 -0400

    Added more rows of data to manipulate

--- Status-Of-Funds-App/src/app/pages/home/home.page.ts
+++ Status-Of-Funds-App/src/app/pages/home/home.page.ts
@@ -12,23 +12,25 @@ import { IonButton, IonIcon, IonInfiniteScroll } from '@ionic/angular';
 })
 export class HomePage implements OnInit {
   @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
-
-  output: JSON;
-  obj: any = {
-    "row1":{"Funded Program: ": "S.0084619.1", "DPI: ": "GFEBS", "SAG: ": "1PLR", "Commitments: ": "1464.3", "Obligation: ": "1464.3", "Disbursements: ": "0", "AOR: ": "FU", "CostCenter: ": "40385520", "Commodity: ": "TRANSPORT_PAX", "TransDate: ": "2021/10/12 00:00:00"},
-    "row2":{"Funded Program: ": "S.0084605.2.9", "DPI: ": "GFEBS", "SAG: ": "1PLR", "Commitments: ": "0", "Obligation: ": "0", "Disbursements: ": "0", "AOR: ": "R8", "CostCenter: ": "40385654", "Commodity: ": "TRANSPORT_EQUIP", "TransDate: ": "2021/10/18 00:00:00"},
-    "row3":{"Funded Program: ": "ARMY", "DPI: ": "GFEBS", "SAG: ": "1PLR", "Commitments: ": "0", "Obligation: ": "0", "Disbursements: ": "0", "AOR: ": "R8", "CostCenter: ": "40385652", "Commodity: ": "TRANSPORT_EQUIP", "TransDate: ": "2021/10/01 00:00:00"},
-    "row4":{"Funded Program: ": "S.0084605.1", "DPI: ": "GFEBS", "SAG: ": "1PLR", "Commitments: ": "14089.67", "Obligation: ": "14089.67", "Disbursements: ": "0", "AOR: ": "R8", "CostCenter: ": "40234828", "Commodity: ": "RENTS, COMMS & UTILITIES", "TransDate: ": "S.0084619.1"},
-    "row5":{"Funded Program: ": "S.0084619.1", "DPI: ": "GFEBS", "SAG: ": "S.0084619.1", "Commitments: ": "S.0084619.1", "Obligation: ": "S.0084619.1", "Disbursements: ": "S.0084619.1", "AOR: ": "R8", "CostCenter: ": "S.0084619.1", "Commodity: ": "S.0084619.1", "TransDate: ": "S.0084619.1"},
-    "row6":{"Funded Program: ": "S.0084619.1", "DPI: ": "GFEBS", "SAG: ": "S.0084619.1", "Commitments: ": "S.0084619.1", "Obligation: ": "S.0084619.1", "Disbursements: ": "S.0084619.1", "AOR: ": "R8", "CostCenter: ": "S.0084619.1", "Commodity: ": "S.0084619.1", "TransDate: ": "S.0084619.1"},
+  
+  output: JSON = <JSON><unknown>{
+    "row0":{"Funded Program: ": "S.0084619.1", "DPI: ": "GFEBS", "SAG: ": "1PLR", "Commitments: ": "1464.3", "Obligation: ": "1464.3", "Disbursements: ": "0", "AOR: ": "FU", "CostCenter: ": "40385520", "Commodity: ": "TRANSPORT_PAX", "TransDate: ": "2021/10/12 00:00:00"},
+    "row1":{"Funded Program: ": "S.0084605.2.9", "DPI: ": "GFEBS", "SAG: ": "1PLR", "Commitments: ": "0", "Obligation: ": "0", "Disbursements: ": "0", "AOR: ": "R8", "CostCenter: ": "40385654", "Commodity: ": "TRANSPORT_EQUIP", "TransDate: ": "2021/10/18 00:00:00"},
+    "row2":{"Funded Program: ": "ARMY", "DPI: ": "GFEBS", "SAG: ": "1PLR", "Commitments: ": "0", "Obligation: ": "0", "Disbursements: ": "0", "AOR: ": "R8", "CostCenter: ": "40385652", "Commodity: ": "TRANSPORT_EQUIP", "TransDate: ": "2021/10/01 00:00:00"},
+    "row3":{"Funded Program: ": "S.0084605.1", "DPI: ": "GFEBS", "SAG: ": "1PLR", "Commitments: ": "14089.67", "Obligation: ": "14089.67", "Disbursements: ": "0", "AOR: ": "R8", "CostCenter: ": "40234828", "Commodity: ": "RENTS, COMMS & UTILITIES", "TransDate: ": "S.0084619.1"},
+    "row4":{"Funded Program: ": "S.0084605.2.9", "DPI: ": "GFEBS", "SAG: ": "1PLR", "Commitments: ": "-300.7", "Obligation: ": "-300.7", "Disbursements: ": "0", "AOR: ": "R8", "CostCenter: ": "40385654", "Commodity: ": "TRANSPORT_EQUIP", "TransDate: ": "2021/10/26 00:00:00"},
+    "row5":{"Funded Program: ": "S.0084605.1", "DPI: ": "GFEBS", "SAG: ": "1PLR", "Commitments: ": "14089.67", "Obligation: ": "14089.67", "Disbursements: ": "0", "AOR: ": "R8", "CostCenter: ": "40234828", "Commodity: ": "RENTS, COMMS, & UTILITIES", "TransDate: ": "2021/10/27 00:00:00"},
+    "row6":{"Funded Program: ": "ARMY", "DPI: ": "GFEBS", "SAG: ": "121", "Commitments: ": "3.36", "Obligation: ": "3.36", "Disbursements: ": "0", "AOR: ": "FU", "CostCenter: ": "40385512", "Commodity: ": "PAYROLL", "TransDate: ": "2021/12/30 00:00:00"},
+    "row7":{"Funded Program: ": "ARMY", "DPI: ": "GCSS-A", "SAG: ": "111", "Commitments: ": "-24.57", "Obligation: ": "-24.57", "Disbursements: ": "0", "AOR: ": "RT", "CostCenter: ": "40385719", "Commodity: ": "PAYROLL", "TransDate: ": "2021/11/23 00:00:00"},
+    "row8":{"Funded Program: ": "S.0087275.1.6", "DPI: ": "", "SAG: ": "1PLV", "Commitments: ": "0", "Obligation: ": "0", "Disbursements: ": "0", "AOR: ": "", "CostCenter: ": "", "Commodity: ": "SVS CONTRACTS", "TransDate: ": ""},
+    "row9":{"Funded Program: ": "", "DPI: ": "58", "SAG: ": "321", "Commitments: ": "0", "Obligation: ": "13902", "Disbursements: ": "0", "AOR: ": "", "CostCenter: ": "", "Commodity: ": "GPC", "TransDate: ": "2022/02/11 00:00:00"}
   };
+
   constructor(private auth: AuthService, private router: Router) { }
 
-  ngOnInit() {}
-   
-   loadDataBase() {
-
-   }
+  ngOnInit() {
+    console.log(this.output);
+  }
 
   loadData(event) {
     setTimeout(() => {
