497364b1b035fa115b991e92a0dfb948cb932bd7
Vinhky Nguyen~er-sama <vinhkynguyen235@gmail.com>
2022-10-30 21:19:05 -0400

    Hard coded some rows of data

--- Status-Of-Funds-App/src/app/pages/home/home.page.ts
+++ Status-Of-Funds-App/src/app/pages/home/home.page.ts
@@ -13,9 +13,22 @@ import { IonButton, IonIcon, IonInfiniteScroll } from '@ionic/angular';
 export class HomePage implements OnInit {
   @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
 
+  output: JSON;
+  obj: any = {
+    "row1":{"Funded Program: ": "S.0084619.1", "DPI: ": "GFEBS", "SAG: ": "1PLR", "Commitments: ": "1464.3", "Obligation: ": "1464.3", "Disbursements: ": "0", "AOR: ": "FU", "CostCenter: ": "40385520", "Commodity: ": "TRANSPORT_PAX", "TransDate: ": "2021/10/12 00:00:00"},
+    "row2":{"Funded Program: ": "S.0084605.2.9", "DPI: ": "GFEBS", "SAG: ": "1PLR", "Commitments: ": "0", "Obligation: ": "0", "Disbursements: ": "0", "AOR: ": "R8", "CostCenter: ": "40385654", "Commodity: ": "TRANSPORT_EQUIP", "TransDate: ": "2021/10/18 00:00:00"},
+    "row3":{"Funded Program: ": "ARMY", "DPI: ": "GFEBS", "SAG: ": "1PLR", "Commitments: ": "0", "Obligation: ": "0", "Disbursements: ": "0", "AOR: ": "R8", "CostCenter: ": "40385652", "Commodity: ": "TRANSPORT_EQUIP", "TransDate: ": "2021/10/01 00:00:00"},
+    "row4":{"Funded Program: ": "S.0084605.1", "DPI: ": "GFEBS", "SAG: ": "1PLR", "Commitments: ": "14089.67", "Obligation: ": "14089.67", "Disbursements: ": "0", "AOR: ": "R8", "CostCenter: ": "40234828", "Commodity: ": "RENTS, COMMS & UTILITIES", "TransDate: ": "S.0084619.1"},
+    "row5":{"Funded Program: ": "S.0084619.1", "DPI: ": "GFEBS", "SAG: ": "S.0084619.1", "Commitments: ": "S.0084619.1", "Obligation: ": "S.0084619.1", "Disbursements: ": "S.0084619.1", "AOR: ": "R8", "CostCenter: ": "S.0084619.1", "Commodity: ": "S.0084619.1", "TransDate: ": "S.0084619.1"},
+    "row6":{"Funded Program: ": "S.0084619.1", "DPI: ": "GFEBS", "SAG: ": "S.0084619.1", "Commitments: ": "S.0084619.1", "Obligation: ": "S.0084619.1", "Disbursements: ": "S.0084619.1", "AOR: ": "R8", "CostCenter: ": "S.0084619.1", "Commodity: ": "S.0084619.1", "TransDate: ": "S.0084619.1"},
+  };
   constructor(private auth: AuthService, private router: Router) { }
 
-  ngOnInit() { }
+  ngOnInit() {}
+   
+   loadDataBase() {
+
+   }
 
   loadData(event) {
     setTimeout(() => {
